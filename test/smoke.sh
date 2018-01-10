50ebf9a958f3460a178b7bf4e3b57dbc6389de9b
Thomas McKee <caffeine.storm@gmail.com>
2018-01-10 14:52:10 -0500

    Using return values in smoke.sh to report success/fail

--- test/smoke.sh
+++ test/smoke.sh
@@ -2,11 +2,16 @@
 
 # Some smoke tests for lsp
 
+ret=0
+
 echo -n "empty program... "
 ./lsp < /dev/null &> /dev/null
 if [[ $? -ne 0 ]]; then
 	echo "FAIL"
+	ret=1
 else
 	echo "PASS"
 fi
+
+exit $ret
 
