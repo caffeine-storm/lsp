357da3989c8045454dc9445d012d35bf9ee38ad1
Thomas McKee <caffeine.storm@gmail.com>
2018-01-30 11:24:46 -0500

    Adding support for string values to the printer

--- src/printer.c
+++ src/printer.c
@@ -9,21 +9,23 @@ void printer_print( printer_type * printer, expr_type * expr, FILE * out ) {
 	(void) printer;
 	switch( expr->type_code ) {
 		case EXPR_TYPE_NIL: {
-			fprintf( out, "n" );
-			return;
-		}
-		case EXPR_TYPE_SYMBOL: {
-			// For now, the only symbol is 'exit'
-			assert( strcmp( expr_get_symbol( expr ), "exit" ) == 0 );
-			fprintf( out, "exit" );
+			fprintf( out, "n\n" );
 			return;
 		}
 		case EXPR_TYPE_INT: {
-			fprintf( out, "%d", expr_get_int( expr ) );
+			fprintf( out, "%d\n", expr_get_int( expr ) );
+			return;
+		}
+		case EXPR_TYPE_STRING: {
+			fprintf( out, "\"%s\"\n", expr_get_string( expr ) );
+			return;
+		}
+		case EXPR_TYPE_SYMBOL: {
+			fprintf( out, "%s\n", expr_get_symbol( expr ) );
 			return;
 		}
 		case EXPR_TYPE_EOF: {
-			fprintf( out, "<EOF>" );
+			fprintf( out, "<EOF>\n" );
 			return;
 		}
 		default: {
