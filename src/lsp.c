65c4dfb5b4f2ea5fe270540904ca014b7526133c
Thomas McKee <caffeine.storm@gmail.com>
2018-01-30 11:28:38 -0500

    Updating the repl to use a namespace and some hard-coded bindings

--- src/lsp.c
+++ src/lsp.c
@@ -1,6 +1,8 @@
 #include <expr.h>
 #include <lsp_assert.h>
 #include <lsp_print.h>
+#include <lsp_util.h>
+#include <namespace.h>
 #include <printer.h>
 #include <reader.h>
 #include <stack.h>
@@ -11,25 +13,54 @@
 
 enum EVAL_RESULT {
 	EVAL_PUSH_EXPR,
+	EVAL_CONTINUE,
 	EVAL_EXIT,
 	EVAL_ERROR
 };
+
+int status_to_rc( int s ) {
+	switch( s ) {
+		case EVAL_PUSH_EXPR:
+		case EVAL_CONTINUE:
+		case EVAL_EXIT:
+			return 0;
+		case EVAL_ERROR:
+			return 1;
+		default:
+			LSP_ABORT( "snh" );
+	}
+}
 
 void print_prompt( FILE * out ) {
 	fprintf( out, "> " );
 }
 
-int eval( stack_type * stack, expr_type * expr ) {
+int eval( stack_type * stack, namespace_type * ns, expr_type * expr ) {
 	(void) stack;
 	switch( expr->type_code ) {
-		case EXPR_TYPE_SYMBOL:
-			// TODO: implement me
-			LSP_ABORT( "niy" );
 		case EXPR_TYPE_EOF:
+			TRACE( "eval: eof-expr" );
 			return EVAL_EXIT;
-		default:
-			return EVAL_PUSH_EXPR;
+		case EXPR_TYPE_SYMBOL: {
+			TRACE( "eval: symbol-expr" );
+			callable_type entry = namespace_get_symbol( ns, expr_get_symbol( expr ) );
+			if( !entry ) {
+				printf( "symbol lookup failure for '%s'\n", expr->val.symbol );
+				return EVAL_ERROR;
+			}
+			(*entry)( stack );
+			return EVAL_CONTINUE;
+		}
+		case EXPR_TYPE_INT:
+		case EXPR_TYPE_STRING:
+			break;
+		default: {
+			TRACE( "eval: unhandled-expr" );
+			LSP_ABORT( "snh" );
+		}
 	}
+
+	return EVAL_PUSH_EXPR;
 }
 
 int main( int argc, char * * argv ) {
@@ -37,14 +68,64 @@ int main( int argc, char * * argv ) {
 	(void) argv;
 
 	int ret;
+	int last_status = EVAL_CONTINUE;
+
+	printer_type printer;
 
 	stack_type stack;
 	stack_init( &stack );
 
+	void print_fn( stack_type * s ) {
+		if( stack_size( s ) < 1 ) {
+			fprintf( stderr, "error: stack underflow\n" );
+			return;
+		}
+		expr_type * e = stack_top( s );
+		printer_print( &printer, e, stdout );
+		stack_pop( s );
+		kill_expr( e );
+	}
+
+	void print_stack( stack_type * s ) {
+		size_t sz = stack_size( s );
+		if( sz == 0 ) {
+			expr_type * e = make_expr();
+			char * s = strdup( "<empty stack>" );
+			expr_set_symbol( e, s );
+			printer_print( &printer, e, stdout );
+			kill_expr( e );
+			return;
+		}
+		while( sz-- ) {
+			expr_type * e = stack_at( s, sz );
+			printer_print( &printer, e, stdout );
+		}
+	}
+
+	void exit_fn( stack_type * s ) {
+		if( stack_size( s ) < 1 ) {
+			fprintf( stderr, "error: stack underflow\n" );
+			return;
+		}
+
+		expr_type * e = stack_top( s );
+		if( e->type_code != EXPR_TYPE_INT ) {
+			fprintf( stderr, "error: can't apply 'exit' to non-int\n" );
+			return;
+		}
+
+		exit( expr_get_int( e ) );
+	}
+
+	namespace_type ns;
+	namespace_init( &ns );
+
+	namespace_bind( &ns, ".", print_fn );
+	namespace_bind( &ns, ":s", print_stack );
+	namespace_bind( &ns, "exit", exit_fn );
+
 	reader_type reader;
 	ret = reader_init( &reader, stdin );
-
-	printer_type printer;
 
 	while( 1 ) {
 		print_prompt( stdout );
@@ -68,10 +149,15 @@ int main( int argc, char * * argv ) {
 				;
 		}
 
-		ret = eval( &stack, expr );
+		ret = eval( &stack, &ns, expr );
 		switch( ret ) {
 			case EVAL_PUSH_EXPR: {
 				stack_push( &stack, expr );
+				break;
+			}
+			case EVAL_CONTINUE: {
+				expr_denit( expr );
+				free( expr );
 				break;
 			}
 			case EVAL_EXIT: {
@@ -79,11 +165,12 @@ int main( int argc, char * * argv ) {
 				free( expr );
 
 				printf( "Goodbye!\n" );
-				exit( 0 );
+				exit( status_to_rc( last_status ) );
 			}
 			case EVAL_ERROR: {
 				// TODO: print an error message that eval will push on the stack
-				continue;
+				printf( "ERROR\n" );
+				break;
 			}
 			default: {
 				assert( 0 && "Invalid eval result (eval internal error)" );
@@ -91,9 +178,13 @@ int main( int argc, char * * argv ) {
 			}
 		}
 
+		last_status = ret;
 		TRACE( "debug: going to print top of stack" );
-		expr_type * top = stack_top( &stack );
-		printer_print( &printer, top, stdout );
+		if( stack_size( &stack ) > 0 ) {
+			expr_type * top = stack_top( &stack );
+			printf( "tos: " );
+			printer_print( &printer, top, stdout );
+		}
 	}
 
 	return 0;
