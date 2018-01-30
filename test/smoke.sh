057763b8ef66f0fff369186e0f5d0268063207fe
Thomas McKee <caffeine.storm@gmail.com>
2018-01-30 11:31:39 -0500

    Adding a few more tests to the smoke-test script

--- test/smoke.sh
+++ test/smoke.sh
@@ -4,14 +4,51 @@
 
 ret=0
 
-echo -n "empty program... "
-./lsp < /dev/null &> /dev/null
-if [[ $? -ne 0 ]]; then
-	echo "FAIL"
-	ret=1
-else
-	echo "PASS"
-fi
+function works() {
+	[[ $1 -eq 0 ]]
+}
+
+function breaks() {
+	[[ $1 -ne 0 ]]
+}
+
+function check_generic() {
+	echo -n "$1... "
+	./lsp &> /dev/null
+	$2 $?
+	if [[ $? -eq 0 ]]; then
+		echo "PASS"
+	else
+		echo "FAIL"
+		ret=1
+	fi
+}
+
+function check() {
+	check_generic "$1" works
+}
+
+function check_error() {
+	check_generic "$1" breaks
+}
+
+check "empty program" < /dev/null
+
+check "return success" <<EOF
+0 exit
+EOF
+
+check_error "return failure" <<EOF
+1 exit
+EOF
+
+check_error "unbound name" <<EOF
+foo
+EOF
+
+check "string literal" <<EOF
+"foo"
+EOF
 
 exit $ret
 
